3793ada02dbb9d2bc3f11aa4c5d8f9ec64638a72
mehak bera <beramehak0@gmail.com>
2026-07-31 13:23:26 +0530

    initial commit

---
+++ src/Componenet/Book.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Book = () => {
+  return (
+    <div style={{border:'2px solid red', height:'400px', width:'400px',backgroundColor:'green'}}>
+      <h1 style={{color:'red'}}>BOOK SHOP</h1>
+      <img src='https://jooinn.com/images/a-stack-of-books-isolated-on-a-white.jpg' alt='book picture' height={'200px'} width={'400px'}></img>
+      <h3>BOOK NAME : COMPUTER BOOK</h3>
+      <h3>PRICE: 600</h3>
+    </div>
+  )
+}
+
+export default Book
